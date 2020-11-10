1885c0e298314945829cbad92ebf3de6f5c62f55
ahmetdanavar <74204791+ahmetdanavar@users.noreply.github.com>
2020-11-10 04:50:07 -0800

    Add files via upload

---
+++ 1new.py
@@ -0,0 +1,90 @@
+import pandas as pd
+df=pd.read_csv("vgsales.csv")
+#%%
+df.drop(['Rank','Name','Platform','Genre','Publisher'],axis=1,inplace=True)
+#%%
+x=df.iloc[:,1::].values
+y=df.iloc[:,0].values
+#%%
+from sklearn.linear_model import LinearRegression
+mlr=LinearRegression()
+mlr.fit(x,y)
+mlr_pred=mlr.predict(x)
+#%%
+from  sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+print("mlr r2:",r2_score(y,mlr_pred))
+print("mlr mae:",mean_absolute_error(y,mlr_pred))
+print("mlr mse:",mean_squared_error(y,mlr_pred))
+#%%
+from sklearn.preprocessing import PolynomialFeatures
+plr=PolynomialFeatures(degree=3)
+
+x_pol=plr.fit_transform(x)
+lr_pol=LinearRegression()
+lr_pol.fit(x_pol,y)
+xnew=plr.fit_transform(x)
+y_pred_pol=lr_pol.predict(xnew)
+#%%
+pol_pred=lr_pol.predict(x_pol)
+
+print("pol r2:",r2_score(y,pol_pred))
+print("pol mae:",mean_absolute_error(y,pol_pred))
+print("pol mse:",mean_squared_error(y,pol_pred))
+#%%
+from  sklearn.tree import  DecisionTreeRegressor
+dt=DecisionTreeRegressor()
+dt.fit(x,y)
+dt_pred=dt.predict(x)
+#%%
+from  sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+print("dt_r2:",r2_score(y,dt_pred))   
+print("mae:",mean_absolute_error(y,dt_pred))
+print("mse:",mean_squared_error(y,dt_pred))
+#%%
+from  sklearn.ensemble import  RandomForestRegressor
+rf=RandomForestRegressor(n_estimators=100,random_state=42)
+rf.fit(x,y.ravel())
+rf_pred=rf.predict(x)
+#%%
+from  sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+print("rf","r2:",r2_score(y,rf_pred),"mae:",mean_absolute_error(y,rf_pred))
+print("mse:",mean_squared_error(y,rf_pred))
+#%%
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.1,random_state=42)
+#%%
+mlr=LinearRegression()
+mlr.fit(x_train,y_train)
+mlr_pred=mlr.predict(x_test)
+print("mlr","r2:",r2_score(y_test,mlr_pred),"mae:",mean_absolute_error(y_test,mlr_pred))
+print("mse:",mean_squared_error(y_test,mlr_pred))
+
+
+
+#%%
+
+dt=DecisionTreeRegressor(random_state=42)
+dt.fit(x_train,y_train)
+dt_pred=dt.predict(x_test)
+print("dt","r2:",r2_score(y_test,dt_pred),"mae:",mean_absolute_error(y_test,dt_pred))
+print("mse:",mean_squared_error(y_test,dt_pred))
+#%%
+rf=RandomForestRegressor(n_estimators=100,random_state=42)
+rf.fit(x_train,y_train.ravel())
+rf_pred=rf.predict(x_test)
+print("rf","r2:",r2_score(y_test,rf_pred),"mae:",mean_absolute_error(y_test,rf_pred))
+print("mse:",mean_squared_error(y_test,rf_pred))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
